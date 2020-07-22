709fe36f988525a3cede86622b12aaaeceb91adc
Austin Chen <akrolsmir@gmail.com>
2020-07-21 20:32:35 -0700

    Render a PDF from an URL on click

--- template/my_component/frontend/src/MyComponent.tsx
+++ template/my_component/frontend/src/MyComponent.tsx
@@ -4,6 +4,7 @@ import {
   StreamlitComponentBase,
   Streamlit,
 } from "./streamlit"
+import PDFJS from "pdfjs-dist"
 
 interface State {
   numClicks: number
@@ -31,17 +32,46 @@ class MyComponent extends StreamlitComponentBase<State> {
         <button onClick={this.onClicked} disabled={this.props.disabled}>
           Click Me!
         </button>
+        <canvas ref="canvas" width={500} height={500} />
       </span>
     )
   }
 
+  private async renderPdf(url: string) {
+    const workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.js`
+    PDFJS.GlobalWorkerOptions.workerSrc = workerSrc
+
+    const pdf = await PDFJS.getDocument(url).promise
+    // Assumes each pdf is one page, for now
+    const page = await pdf.getPage(1)
+
+    const scale = 1.3
+    const viewport = page.getViewport({ scale })
+
+    const canvas = this.refs.canvas as HTMLCanvasElement
+    const ctx = canvas.getContext("2d")
+    // Resize canvas to fit the pdf page
+    canvas.height = viewport.height
+    canvas.width = viewport.width
+
+    const renderContext = {
+      canvasContext: ctx!,
+      viewport: viewport,
+    }
+    page.render(renderContext)
+  }
+
   /** Click handler for our "Click Me!" button. */
-  private onClicked = (): void => {
+  private onClicked = async () => {
     // Increment state.numClicks, and pass the new value back to
     // Streamlit via `Streamlit.setComponentValue`.
     this.setState(
-      prevState => ({ numClicks: prevState.numClicks + 1 }),
+      (prevState) => ({ numClicks: prevState.numClicks + 1 }),
       () => Streamlit.setComponentValue(this.state.numClicks)
+    )
+    await this.renderPdf(
+      "https://cors-anywhere.herokuapp.com/" +
+        "http://puzzledpint.com/files/8315/3168/9544/05-science-v6.pdf"
     )
   }
 }
