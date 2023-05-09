aa72e7f26ebd8b22ba2df627da0898442432108d
Kamil Bregula <kamil.bregula@snowflake.com>
2023-05-09 16:46:59 +0200

    Fix material login example

--- examples/MaterialLogin/frontend/src/MaterialLogin.tsx
+++ examples/MaterialLogin/frontend/src/MaterialLogin.tsx
@@ -7,12 +7,12 @@ import {
 import * as Yup from 'yup'
 import {Formik, Form, Field} from 'formik'
 
-import Card from '@material-ui/core/Card'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
+import Card from '@mui/material/Card'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
 
 import styles from './style.module.scss'
 
