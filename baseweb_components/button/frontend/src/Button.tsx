342806630aaae4f9138e3c2238f00b975c27c5ae
Thomas Bouamoud <thomasbs17@yahoo.fr>
2023-05-14 19:33:46 +0100

    working on button implementation

--- baseweb_components/button/frontend/src/Button.tsx
+++ baseweb_components/button/frontend/src/Button.tsx
@@ -1,21 +1,47 @@
 import {
+    Streamlit,
     StreamlitComponentBase,
     withStreamlitConnection,
 } from "streamlit-component-lib"
 import React, { ReactNode } from "react"
-import { Button } from "baseui/button";
+import { Button, SIZE, SHAPE, KIND } from "baseui/button";
 
 interface State {
-    numClicks: number
-    isFocused: boolean
+    label: string,
+    size: string,
+    shape: string,
+    isLoading: boolean,
+    isSelected: boolean,
+    disabled: boolean,
+    kind: string
 }
 
+
 class BaseWebButton extends StreamlitComponentBase<State> {
+    public state = { label: 'Button', size: SIZE.default, shape: SHAPE.default, isLoading: false, isSelected: false, disabled: false, kind: KIND.primary }
+
+
     public render = (): ReactNode => {
+        const label = this.props.args["label"]
+        const disabled = this.state.disabled
+        const { theme } = this.props
+
         return (
-            <Button onClick={() => alert("click")}>Hello</Button>
+            <Button
+                disabled
+                onClick={this.onClicked}>
+                {label}
+            </Button>
         );
     }
+
+    private onClicked = (): void => {
+        Streamlit.events.dispatchEvent();
+        console.log('you clicked')
+    }
+
 }
+
+
 
 export default withStreamlitConnection(BaseWebButton)
