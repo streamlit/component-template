133636f49a1fe3c2d99285e68afb6d2fd3322770
Kamil Bregula <kamil.bregula@snowflake.com>
2023-06-22 10:06:01 +0200

    Add cookiecutter template

---
+++ cookiecutter/README.md
@@ -0,0 +1,18 @@
+# Streamlit template for CookieCutter
+
+Template for creating a streamlit component
+
+## Usage
+
+Make sure you have [https://pypi.org/project/cookiecutter/](cookiecutter) installed. To install it, you can use [pipx](https://pypa.github.io/pipx/):
+```sh
+pipx install cookiecutter
+```
+
+Then run
+// TODO: Remove --checkout cookieuctter before merge
+```
+cookiecutter https://github.com/streamlit/component-template.git --directory=cookiecutter --checkout cookieuctter
+```
+
+And follow the instructions
--- template/setup.py
+++ template/setup.py
@@ -1,18 +1,23 @@
+from pathlib import Path
+
 import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-custom-component",
     version="0.0.1",
-    author="",
-    author_email="",
-    description="",
-    long_description="",
-    long_description_content_type="text/plain",
+    author="John Smith",
+    author_email="john@example.com",
+    description="Streamlit component that allows you to do X",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         # By definition, a Custom Component depends on Streamlit.
         # If your component has other Python dependencies, list
