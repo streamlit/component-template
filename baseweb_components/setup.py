f1e78250769549857bc8d6b43581eb92cf4e7b47
Thomas Bouamoud <thomasbs17@yahoo.fr>
2023-07-03 18:59:49 +0100

    - included frontend dir - added changelog file

--- baseweb_components/setup.py
+++ baseweb_components/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_baseweb",
-    version="0.0.2",
+    version="0.0.3",
     author="Thomas Bouamoud",
     author_email="thomas.bouamoud@gmail.com",
     description="A Streamlit implementation of the Base Web library.",
