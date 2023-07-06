6bbb2fadd92da1d2828fd03e2b2cbd110342d690
Thomas Bouamoud <thomasbs17@yahoo.fr>
2023-07-06 01:16:29 +0100

    bumped version to reflect updated README.md on Pypi

--- baseweb_components/setup.py
+++ baseweb_components/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_baseweb",
-    version="0.1.0",
+    version="0.1.1",
     author="Thomas Bouamoud",
     author_email="thomas.bouamoud@gmail.com",
     description="A Streamlit implementation of the Base Web library.",
