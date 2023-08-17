0528ad2222aba69081c12cb826637f1e4b7580dc
Przemysław Bełczyk <przemyslaw.belczyk@snowflake.com>
2023-08-17 16:02:34 +0200

    Revert example of template-reactless

--- template-reactless/my_component/example.py
+++ template-reactless/my_component/example.py
@@ -9,11 +9,11 @@ st.subheader("Component with constant args")
 
 # Create an instance of our component with a constant `name` arg, and
 # print its output value.
-# num_clicks = my_component("World")
-# st.markdown("You've clicked %s times!" % int(num_clicks))
-#
-# st.markdown("---")
-# st.subheader("Component with variable args")
+num_clicks = my_component("World")
+st.markdown("You've clicked %s times!" % int(num_clicks))
+
+st.markdown("---")
+st.subheader("Component with variable args")
 
 # Create a second instance of our component whose `name` arg will vary
 # based on a text_input widget.
@@ -23,6 +23,6 @@ st.subheader("Component with constant args")
 # it is considered a new instance and will be re-mounted on the frontend
 # and lose its current state. In this case, we want to vary the component's
 # "name" argument without having it get recreated.
-# name_input = st.text_input("Enter a name", value="Streamlit")
-# num_clicks = my_component(name_input, key="foo")
-# st.markdown("You've clicked %s times!" % int(num_clicks))
+name_input = st.text_input("Enter a name", value="Streamlit")
+num_clicks = my_component(name_input, key="foo")
+st.markdown("You've clicked %s times!" % int(num_clicks))
