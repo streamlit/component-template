133bd726ba6ec8bacacc606054a5da02fef72853
Bob Nisco <bob.nisco@snowflake.com>
2025-04-16 09:03:33 -0700

    Better Type Safety

--- template/my_component/frontend/src/index.tsx
+++ template/my_component/frontend/src/index.tsx
@@ -1,10 +1,17 @@
-import React from "react"
+import React, { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import MyComponent from "./MyComponent"
 
-const root = createRoot(document.getElementById("root") as HTMLElement)
+const rootElement = document.getElementById("root")
+
+if (!rootElement) {
+  throw new Error("Root element not found")
+}
+
+const root = createRoot(rootElement)
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <MyComponent />
-  </React.StrictMode>
+  </StrictMode>
 )
