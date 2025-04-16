c9acb83b926ab388faf80f3ed8ee788291225700
Bob Nisco <bob.nisco@snowflake.com>
2025-04-16 11:23:31 -0700

    Update MaterialLogin

--- examples/MaterialLogin/material_login/frontend/src/MaterialLogin.tsx
+++ examples/MaterialLogin/material_login/frontend/src/MaterialLogin.tsx
@@ -1,91 +1,108 @@
-import React, {useEffect, ReactElement} from "react"
+import { useFormik } from "formik"
+import { ReactElement, useEffect } from "react"
 import {
-  withStreamlitConnection,
-  Streamlit,
   ComponentProps,
+  Streamlit,
+  withStreamlitConnection,
 } from "streamlit-component-lib"
-import * as Yup from 'yup'
-import {Formik, Form, Field} from 'formik'
+import * as Yup from "yup"
 
-import Card from '@material-ui/core/Card'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
+import Button from "@mui/material/Button"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import TextField from "@mui/material/TextField"
+import Typography from "@mui/material/Typography"
 
-import styles from './style.module.scss'
+import styles from "./style.module.scss"
 
-function LoginComponent({args}: ComponentProps): ReactElement {
-  const {title} = args
+interface LoginFormValues {
+  username: string
+  password: string
+}
 
-  const initialValues = {
-    username: '',
-    password: '',
-  }
-  const validationSchema = Yup.object({
-    username: Yup.string().email().required(),
-    password: Yup.string().required(),
-  })
+const validationSchema = Yup.object({
+  username: Yup.string().email().required(),
+  password: Yup.string().required(),
+})
+
+function LoginComponent({ args }: ComponentProps): ReactElement {
+  const { title } = args
 
   useEffect(() => {
     Streamlit.setFrameHeight()
   })
 
-  const onSubmit = (values: any, {setSubmitting}: any) => {
-    setSubmitting(false)
-    Streamlit.setComponentValue(values)
-  }
-
-  const onReset = () => Streamlit.setComponentValue({})
+  const formik = useFormik<LoginFormValues>({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values, { setSubmitting }) => {
+      setSubmitting(false)
+      Streamlit.setComponentValue(values)
+    },
+    onReset: () => {
+      Streamlit.setComponentValue({})
+    },
+  })
 
   return (
-    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} onReset={onReset}>
-      {({errors, isSubmitting}: any) => (
-        <Form className={styles.loginForm}>
-          <Card>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {title}
-              </Typography>
+    <form
+      onSubmit={formik.handleSubmit}
+      onReset={formik.handleReset}
+      className={styles.loginForm}
+    >
+      <Card>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
 
-              <Field
-                name="username"
-                placeholder="username@domain.com"
-                as={TextField}
-                fullWidth
-                margin="dense"
-                error={Boolean(errors.username)}
-                helperText={errors.username}
-              />
+          <TextField
+            id="username"
+            name="username"
+            label="Username"
+            placeholder="username@domain.com"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.username && Boolean(formik.errors.username)}
+            helperText={formik.touched.username && formik.errors.username}
+            fullWidth
+            margin="dense"
+          />
 
-              <Field
-                type="password"
-                name="password"
-                placeholder="your password"
-                as={TextField}
-                fullWidth
-                margin="dense"
-                error={Boolean(errors.password)}
-                helperText={errors.password}
-              />
-            </CardContent>
-            <CardActions>
-              <Button
-                type="submit"
-                color="primary"
-                disabled={isSubmitting || Object.keys(errors).length > 0}
-              >
-                Login
-              </Button>
-              <Button type="reset" color="secondary">
-                Cancel
-              </Button>
-            </CardActions>
-          </Card>
-        </Form>
-      )}
-    </Formik>
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            placeholder="your password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            fullWidth
+            margin="dense"
+          />
+        </CardContent>
+        <CardActions>
+          <Button
+            type="submit"
+            color="primary"
+            disabled={formik.isSubmitting || !formik.isValid}
+          >
+            Login
+          </Button>
+          <Button type="reset" color="secondary">
+            Cancel
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
   )
 }
 
